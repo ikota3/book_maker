9c0e173e668a3bba0377a9471802d85d7c03b5ff
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-14 23:59:32 +0900

    Add handler and watch

---
+++ src/handler/handler.py
@@ -0,0 +1,20 @@
+﻿from watchdog.events import PatternMatchingEventHandler
+
+
+class Handler(PatternMatchingEventHandler):
+    def __init__(self, patterns=['*.pdf']):
+        super(Handler, self).__init__(patterns=patterns,
+                                      ignore_directories=True,
+                                      case_sensitive=False)
+
+    def on_created(self, event):
+        print('created')
+
+    def on_deleted(self, event):
+        print('deleted')
+
+    def on_moved(self, event):
+        print('moved')
+
+    def on_modified(self, event):
+        print('modified')
---
+++ src/watch.py
@@ -0,0 +1,31 @@
+﻿import sys
+import time
+import subprocess
+from watchdog.observers import Observer
+from handler.handler import Handler
+
+
+def watch(path, extensions):
+    print(['*.%s' % extension for extension in extensions])
+    event_handler = Handler(['*.%s' % extension for extension in extensions])
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    print('--Start Observer--')
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.unschedule_all()
+        observer.stop()
+        print('--End Observer--', flush=True)
+    observer.join()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: ', '%s dir_to_watch [extensions]' % sys.argv[0])
+    elif len(sys.argv) == 2:
+        watch(sys.argv[1], ['pdf'])
+    else:
+        watch(sys.argv[1], sys.argv[2:])

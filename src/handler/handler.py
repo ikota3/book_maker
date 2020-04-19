b6a1e225b32a95f1b57c00de6b6fa6bc6df05bf2
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-19 19:46:53 +0900

    Add some behavior to handler to get isbn from pdf

--- src/handler/handler.py
+++ src/handler/handler.py
@@ -1,33 +1,37 @@
 ﻿import os
 import subprocess
+from src.isbn_from_pdf import get_isbn_from_pdf, NoSuchISBNException
 from watchdog.events import PatternMatchingEventHandler
 
 
 class Handler(PatternMatchingEventHandler):
-    def __init__(self, patterns=['*.pdf']):
+    def __init__(self, patterns=None):
+        if patterns is None:
+            patterns = ['*.pdf']
         super(Handler, self).__init__(patterns=patterns,
                                       ignore_directories=True,
                                       case_sensitive=False)
 
     def on_created(self, event):
         print('!Create Event!')
-        file_path = os.path.join(os.path.dirname(__file__), '../../getISBN.sh')
-        cmd = f'{file_path} {event.src_path}'
-        # result = subprocess.call(cmd, shell=True)
+        shell_path = os.path.join(os.path.dirname(__file__), '../../getISBN.sh')
+        pdf_path = event.src_path
+        cmd = f'{shell_path} {pdf_path}'
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
         if result.returncode == 0:
             # Retrieve ISBN
             isbn = result.stdout.strip()
-            print('isbn -> ' + isbn.strip())
-        else:
-            # Get isbn from pdf text
-            print('cannot get isbn')
+            print(f'isbn from shell -> {isbn}')
+            # TODO rename file to isbn.pdf
 
-    # def on_deleted(self, event):
-    #     print('!Delete Event!')
-    #
-    # def on_moved(self, event):
-    #     print('!Move Event!')
-    #
-    # def on_modified(self, event):
-    #     print('!Modify Event!')
+        else:
+            try:
+                # Get isbn from pdf barcode or text
+                isbn = get_isbn_from_pdf(pdf_path)
+                print(f'isbn from python -> {isbn}')
+                # TODO rename file to isbn.pdf
+
+            except NoSuchISBNException as e:
+                print(e.args[0])
+                print(f'Move {os.path.basename(pdf_path)} to temporary folder.')
+                # TODO move file to tmp dir

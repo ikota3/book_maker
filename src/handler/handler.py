dd8dd022b9fbd7c9f053e11520694fc79d6294a5
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-08-29 01:12:19 +0900

    Update handler for adding doc

--- src/handler/handler.py
+++ src/handler/handler.py
@@ -1,4 +1,10 @@
-﻿import os
+﻿"""ディレクトリ監視ハンドラー
+
+ディレクトリを監視する際の操作内容．
+
+"""
+
+import os
 import shutil
 import datetime
 import subprocess
@@ -9,6 +15,17 @@ from src.constants.log_constants import Message, LogStatus
 
 
 class Handler(PatternMatchingEventHandler):
+    """パターンマッチングハンドラー
+
+    渡された引数を使い，その拡張子を持ったファイルを使って各種操作を行う．
+
+    Attributes:
+        queue (obj: `queue`): GUI側に引き渡すためのキュー
+        input_path (str): 入力ディレクトリ
+        output_path (str): 出力ディレクトリ
+        patterns (list[str]): 拡張子パターン
+    """
+
     def __init__(self, queue, input_path, output_path, patterns=None):
         if patterns is None:
             patterns = ['*.pdf']
@@ -17,29 +34,48 @@ class Handler(PatternMatchingEventHandler):
                                       case_sensitive=False)
         self.queue = queue
         self.input_path = input_path
-        # If the output_path is equal to input_path, then make a directory named with current time
+
+        # 入力ディレクトリと出力ディレクトリが同じとき，
+        # 出力ディレクトリとして現在時刻を使って新たにディレクトリを作成する
         if input_path == output_path:
-            self.output_path = os.path.join(self.input_path, datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
+            self.output_path = os.path.join(
+                self.input_path,
+                datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+            )
         else:
             self.output_path = output_path
         os.makedirs(self.output_path, exist_ok=True)
 
-        # Create tmp directory inside of output directory
+        # tmpディレクトリを出力ディレクトリ内部に作成する
         self.tmp_path = os.path.join(self.output_path, 'tmp')
         os.makedirs(self.tmp_path, exist_ok=True)
 
     def __del__(self):
-        # Delete the tmp directory, when the directory is empty
+        """後処理を行う
+
+        出力ディレクトリの中に作成したtmpディレクトリの後始末や，
+        出力ディレクトリ自体の後始末を行う．
+
+        """
+        # tmpディレクトリが空のとき，tmpディレクトリを削除する
         tmp_files_len = len(os.listdir(self.tmp_path))
         if tmp_files_len == 0:
             os.rmdir(self.tmp_path)
 
-        # Delete the output directory, when the directory is empty
+        # 出力ディレクトリが空のとき，出力ディレクトリを削除する
         output_files_len = len(os.listdir(self.output_path))
         if output_files_len == 0:
             os.rmdir(self.output_path)
 
     def _book_info_from_each_api(self, isbn, event_src_path):
+        """各APIを使って本の情報を取得
+
+        Google Books API，OPENBDを使って，本の情報を取得する．
+
+        Args:
+            isbn (str): ISBNコード
+            event_src_path (str): 情報を取得する本のファイルパス
+        """
         google_book_info = None
         try:
             google_book_info = book_info_from_google(isbn)
@@ -72,12 +108,26 @@ class Handler(PatternMatchingEventHandler):
             self._rename_and_move_pdf(openbd_book_info, event_src_path)
 
     def _rename_and_move_pdf(self, book_info, event_src_path):
-        # Rename pdf file to formatted name
-        pdf_rename_path = os.path.join(os.path.dirname(event_src_path), f'[{book_info.author}]{book_info.title}.pdf')
+        """本の情報を使ってファイル名を変更
+
+        本の情報を使ってファイル名を変更し，出力ディレクトリに移動させる．
+
+        Args:
+            book_info (obj: `BookInfo`): 本の情報
+            event_src_path (str): リネーム対象ファイルパス
+        """
+        # ファイル名を本の情報を使って変更する
+        pdf_rename_path = os.path.join(
+            os.path.dirname(event_src_path),
+            f'[{book_info.author}]{book_info.title}.pdf'
+        )
         os.rename(event_src_path, pdf_rename_path)
 
-        # If pdf file already exists, move file to tmp directory
-        output_path_with_basename = os.path.join(self.output_path, os.path.basename(pdf_rename_path))
+        # 出力ディレクトリに同じファイル名のものがあるとき，tmpフォルダに移動させる
+        output_path_with_basename = os.path.join(
+            self.output_path,
+            os.path.basename(pdf_rename_path)
+        )
         if os.path.isfile(output_path_with_basename):
             shutil.move(pdf_rename_path, self.tmp_path)
             self.queue.put(
@@ -96,13 +146,29 @@ class Handler(PatternMatchingEventHandler):
             )
 
     def on_created(self, event):
-        shell_path = os.path.join(os.path.dirname(__file__), '../../getISBN.sh')
+        """作成イベント感知メソッド
+
+        `__init__`で定義したパターンに従ったファイルが作成されたイベントを感知し，
+        ISBNコードを使ってファイル名を正しい本の題名に変更させる．
+
+        Args:
+            event (obj: `watchdog.events.DirCreatedEvent` or `watchdog.events.FileCreatedEvent`): イベント情報
+        """
+        shell_path = os.path.join(
+            os.path.dirname(__file__),
+            '../../getISBN.sh'
+        )
         event_src_path = event.src_path
         cmd = f'{shell_path} {event_src_path}'
-        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        result = subprocess.run(
+            cmd,
+            shell=True,
+            capture_output=True,
+            text=True
+        )
         try:
             if result.returncode == 0:
-                # Retrieve ISBN from shell
+                # ISBNコードをシェルから取得
                 isbn = result.stdout.strip()
                 self.queue.put(
                     Message(
@@ -113,7 +179,7 @@ class Handler(PatternMatchingEventHandler):
                 self._book_info_from_each_api(isbn, event_src_path)
 
             else:
-                # Get ISBN from pdf barcode or text
+                # ISBNコードをバーコードから，または文字列から取得
                 isbn = get_isbn_from_pdf(event_src_path)
                 self.queue.put(
                     Message(
@@ -124,11 +190,11 @@ class Handler(PatternMatchingEventHandler):
                 self._book_info_from_each_api(isbn, event_src_path)
 
         except (NoSuchISBNException, NoSuchOCRToolException) as e:
-            # NoSuchISBNException will be thrown when the shell command has failure and cannot find isbn using python.
+            # ISBNコードが見つからなかったとき
             if isinstance(e, NoSuchISBNException):
                 self.queue.put(Message(LogStatus.WARNING, e.args[0]))
 
-            # raise the error. because without OCR tool, cannot extract ISBN from image
+            # OCRツールが見つからなかったとき
             if isinstance(e, NoSuchOCRToolException):
                 self.queue.put(Message(LogStatus.ERROR, e.args[0]))
                 raise
@@ -137,6 +203,4 @@ class Handler(PatternMatchingEventHandler):
             self.queue.put(
                 Message(
                     LogStatus.WARNING,
-                    f'Move {os.path.basename(event_src_path)} to {self.tmp_path}'
-                )
-            )
+                    f'Move {os.path.basename(event_src_path)} to {self.tmp_path}'))
